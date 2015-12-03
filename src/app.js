3055756a9a3d93016c29ebdd7fc022ecb07c6103
Alex Wilmer <djazium@gmail.com>
2015-12-03 02:58:00 -0500

    Step 2: Hello Rectangle

--- src/app.js
+++ src/app.js
@@ -10,3 +10,19 @@ function fitWindow () {
 fitWindow()
 
 window.onresize = fitWindow
+
+// The fun part!
+
+let context = canvas.getContext(`2d`)
+
+function render() {
+  context.beginPath()
+  context.fillStyle = `hsl(180, 75%, 50%)`
+  context.rect(
+    0, 0,
+    100, 100
+  )
+  context.fill()
+}
+
+render()
