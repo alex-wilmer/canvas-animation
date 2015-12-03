3a3d53866dfe891147e23b5147f9e472cabb2152
Alex Wilmer <djazium@gmail.com>
2015-12-02 23:22:58 -0500

    whatever

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
-let msg = document.createElement(`h1`)
-msg.innerText = `Hello world!`
-document.body.appendChild(msg)
+let canvas =
+  document.querySelector(`canvas`)
+
+canvas.width =
+  window.innerWidth
+
+canvas.height =
+  window.innerHeight
+
+let context =
+  canvas.getContext(`2d`)
+
+let render = (t) => {
+  t += 1000
+
+  console.log(t)
+
+  let height = Math.abs(Math.sin(t)) * 500
+
+  console.log(height)
+
+  context.fillStyle = `rgb(200,0,0)`
+  context.fillRect(0, 0, height, 100)
+
+  return requestAnimationFrame(render)
+}
+
+render()
