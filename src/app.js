3fe4d75a6b651a2173f1f21c66225a6a50f338e9
Alex Wilmer <djazium@gmail.com>
2015-12-04 17:36:24 -0500

    Step 8: Sin

--- src/app.js
+++ src/app.js
@@ -16,24 +16,24 @@ window.onresize = fitWindow
 let context = canvas.getContext(`2d`)
 
 function render (time) {
-  time = time / 10
+  time = time / 200
   canvas.width = canvas.width
 
   let hue = time % 360
 
-  let position = [
-    time * 5 % canvas.width,
-    time * 5 % canvas.height
+  let coordinates = [
+    canvas.width / 2,
+    canvas.height / 2
   ]
 
-  let size = time % 100 // radians
+  let radius = Math.max(0, Math.sin(time)) * 100
   let angle = [ 0, 360 ]
 
   context.beginPath()
   context.fillStyle = `hsl(${hue}, 75%, 50%)`
   context.arc(
-    ...position,
-    size,
+    ...coordinates,
+    radius,
     ...angle
   )
   context.fill()
