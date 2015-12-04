a5a7faaa13eaf811d51f057bcf73c9a1f371e9fa
Alex Wilmer <djazium@gmail.com>
2015-12-04 18:18:53 -0500

    Step 11: Trip Out

--- src/app.js
+++ src/app.js
@@ -19,17 +19,17 @@ function render (time) {
   time = time / 200
   canvas.width = canvas.width
 
-  let dots = 20
+  let dots = 300
 
   let originX = canvas.width / 2
   let originY = canvas.height / 2
 
   for (let i = 0; i < dots; i += 1) {
-    let hue = time * i % 360
+    let hue = time * i / 10 % 360
 
     let coordinates = [
-      originX + Math.sin(i) * 200,
-      originY + Math.cos(i) * 200
+      originX + Math.sin(i) * i,
+      originY + Math.cos(i) * i
     ]
 
     let radius = Math.max(0, Math.sin(time + i / 10)) * 100
