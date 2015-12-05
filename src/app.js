575499754e3ebc4083be6c2c1111ddb5aa2f3f3e
Alex Wilmer <djazium@gmail.com>
2015-12-04 20:24:46 -0500

    merge demo version

--- src/app.js
+++ src/app.js
@@ -11,37 +11,48 @@ window.onresize = handleResize
 
 handleResize()
 
+let mouse = { x: 0, y: 0 }
+
+window.onmouseover = event =>
+  mouse = { x: event.clientX, y: event.clientY }
+
+window.onmousemove = event =>
+  mouse = { x: event.clientX, y: event.clientY }
+
 // the fun part
 
 let context = canvas.getContext(`2d`)
 
 function render (time) {
+  time = time % 100000
+
   canvas.width = canvas.width
 
   let originX = canvas.width / 2
   let originY = canvas.height / 2
 
-  let dots = 360
+  let dots = mouse.x
 
   for (let i = 0; i < dots; i += 1) {
-
     let coordinates = [
       originX + Math.sin(i) * i,
       originY + Math.cos(i) * i
     ]
 
-    let radius = Math.max(0, Math.sin(time * i / 30000)) * (i / 10) 
+    let radius =
+      Math.max(0, Math.sin(time * i / 30000)) * i / 10
+
     let angle = [ 0, Math.PI * 2 ]
 
-    let hue = time / i % 360
+    let hue = time / i * (mouse.y / 210)
 
     context.beginPath()
-    context.fillStyle = `hsl(${ hue }, 67%, 51%)`
     context.arc(
       ...coordinates,
       radius,
       ...angle
     )
+    context.fillStyle = `hsl(${ hue }, 67%, 51%)`
     context.fill()
   }
 
